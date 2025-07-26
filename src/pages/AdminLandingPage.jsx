52cd50f7c73dcea92df6e913d025f0283c8830d2
chetan <vhanmanechetan17@gmail.com>
2025-07-27 00:00:59 +0530

    improved code

--- src/pages/AdminLandingPage.jsx
+++ src/pages/AdminLandingPage.jsx
@@ -11,12 +11,41 @@ import {
   UserGroupIcon,
   WrenchIcon,
 } from "@heroicons/react/24/solid";
+const cardData=[
+  {
+    icon: UserGroupIcon,
+    title:"Total Users",
+    value:"1500"
+  },
+  {
+    icon: WrenchIcon,
+    title:"Professionals",
+    value:"500"
+  },
+  {
+    icon: ClipboardDocumentListIcon,
+    title:"Bookings Today",
+    value:"1500"
+  },{
+    icon:BanknotesIcon,
+    title:"Total Revenue",
+    value:""
 
+  }
+]
 export const AdminLandingPage = () => {
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-6">
-        <OverviewCard icon={UserGroupIcon} title="Total Users" value="1500" />
+         {cardData.map((data, index) => (
+        <OverviewCard
+          key={index}
+          icon={data.icon}
+          title={data.title}
+          value={data.value}
+        />
+      ))}
+        {/* <OverviewCard icon={UserGroupIcon} title="Total Users" value="1500" />
         <OverviewCard icon={WrenchIcon} title="Professionals" value="500" />
         <OverviewCard
           icon={ClipboardDocumentListIcon}
@@ -27,7 +56,7 @@ export const AdminLandingPage = () => {
           icon={BanknotesIcon}
           title="Total Revenue"
           value="$120K"
-        />
+        /> */}
       </div>
       <div className="flex flex-col gap-y-4">
         <div className="grid grid-cols-12 gap-4">
